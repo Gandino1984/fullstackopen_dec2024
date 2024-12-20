4d93b2ed0220f505874d2efa1b9f2c67217b9493
Gandino1984 <andinogerman@gmail.com>
2024-12-20 07:33:22 +0100

    1.1 & 1.2 done

--- part1/1.1-1.5/src/App.jsx
+++ part1/1.1-1.5/src/App.jsx
@@ -1,26 +1,52 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+import React from 'react'
 
-  
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({ parts }) => {
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      {parts.map((part, index) => (
+        <p key={index}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+  )
+}
+
+const Total = ({ total }) => {
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  // course name
+  const course = 'Half Stack application development'
+
+  //parts of the course
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ]
+
+  //total number of exercises
+  let total=0;
+
+  for (const part of parts) {
+    total += part.exercises;
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total total={total} />
     </div>
   )
 }
